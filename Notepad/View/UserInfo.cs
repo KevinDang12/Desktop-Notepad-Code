668a5f9155fead952c3f01faa446dd090f302669
Kevin Dang <kevin.dang@sheridancollege.ca>
2025-05-14 22:39:09 -0400

    Updated styling and gitignore

--- Notepad/View/UserInfo.cs
+++ Notepad/View/UserInfo.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Notepad.View
+﻿namespace Notepad.View
 {
     /// <summary>
     /// The User Info for getting the user's notes
